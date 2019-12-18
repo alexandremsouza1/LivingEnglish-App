264d8722bb3c26fd835df4d403a05beb34725564
alexandremsouza1 <pedestaljunior@gmail.com>
2019-12-18 10:01:47 -0300

    new component lyrics

---
+++ src/pages/lyrics.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div >
+    <q-input
+      v-model="text"
+      type="textarea"
+      :max-height="100" 
+      rows="100"
+    />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      text: ''
+    }
+  }
+}
+</script>
