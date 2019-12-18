4b7f9f33e5a0ed573e7bed765ec61d80ee3441fe
alexandremsouza1 <pedestaljunior@gmail.com>
2019-12-18 10:00:55 -0300

    componentes letras

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -28,13 +28,13 @@
     >
       <q-list>
         <q-item-label header>Essential Links</q-item-label>
-        <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
+        <q-item clickable to='/lyrics/'>
           <q-item-section avatar>
-            <q-icon name="school" />
+            <q-icon name="library_music" />
           </q-item-section>
           <q-item-section>
-            <q-item-label>Docs</q-item-label>
-            <q-item-label caption>quasar.dev</q-item-label>
+            <q-item-label>Letras</q-item-label>
+            <q-item-label caption>Cole sua letra aqui</q-item-label>
           </q-item-section>
         </q-item>
         <q-item clickable tag="a" target="_blank" href="https://github.quasar.dev">
