c255ff0f046279a9e06dfa0223754c592e638a57
alexandremsouza1 <pedestaljunior@gmail.com>
2020-01-10 08:07:27 -0300

    evento paste finalizado

--- src/pages/lyrics.vue
+++ src/pages/lyrics.vue
@@ -6,6 +6,7 @@
       type="textarea"
       :max-height="100" 
       rows="100"
+      @paste.native="paste"
     />
     </div>
   </div>
@@ -16,6 +17,24 @@ export default {
     return {
       text: ''
     }
-  }
+  },
+  methods:{
+    paste(e) {
+        let paste = e.clipboardData.getData('text')
+        var lyrics = [];
+        setTimeout(() => {
+            var arr = [];
+            arr = paste.split("\n");
+            arr.forEach(function(x,y){
+                if (y % 2 == 0){
+                  lyrics.push(x)
+                }else{
+                  lyrics.push("\n\n");
+                }
+            })
+            this.text = lyrics.join(" ");
+        }, 1000);
+      }
+    }
 }
 </script>
