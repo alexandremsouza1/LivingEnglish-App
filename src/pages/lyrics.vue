96eef44426aecd4f08e44ad90ce1c21f9055646c
alexandremsouza1 <pedestaljunior@gmail.com>
2020-01-16 18:27:39 -0300

    page letras traduzindo e falando

--- src/pages/lyrics.vue
+++ src/pages/lyrics.vue
@@ -9,9 +9,18 @@
       @paste.native="paste"
     />
     </div>
+    <q-btn
+      round
+      color="primary"
+      @click="verify"
+      class="fixed"
+      icon="add"
+      style="right: 18px; bottom: 18px"
+    />
   </div>
 </template>
 <script>
+import {t,falar} from 'src/plugins/translate.js'
 export default {
   data () {
     return {
@@ -34,7 +43,27 @@ export default {
             })
             this.text = lyrics.join(" ");
         }, 1000);
-      }
+    },
+    verify(){
+       var __self = this;
+       var arr = [];
+       arr = this.text.split("\n");
+        arr.forEach(async function(x,y){
+          debugger
+          if (!(y % 2 == 0) && ( x.trim() != '')){
+            try{
+              await t(arr[y-1]).then(function(result){
+                arr[y] = result
+                __self.text = arr.join("\n");
+              })
+            falar(arr[y-1])
+            }catch(err){
+              console.log(err);
+            }
+          }
+        })
+        
     }
+  }
 }
 </script>
